5f7a4b43b5b999b56903cc935a3f86257c500c36
dinelka <newlakshan3@gmail.com>
2025-02-13 20:47:07 +0530

    update field router

--- src/routers/FieldRouter.ts
+++ src/routers/FieldRouter.ts
@@ -4,8 +4,8 @@ import {deleteFieldByCode, getAllField, getFieldById, saveField, updateFieldDta}
 
 const router=express.Router();
 router.post("/saveField",saveField);
-router.get("/getAllField",getAllField);
-router.get("/:id",getFieldById);
+router.get("/getAllFields",getAllField);
+router.get("/getFieldByCode/:id",getFieldById);
 router.put("/updateField/:code",updateFieldDta);
 router.delete("/deleteField/:code",deleteFieldByCode)
 
